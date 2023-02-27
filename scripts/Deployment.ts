9d7c8e56a1fca37bd4ebe9ad73483dd7236959af
subbu0x0 <subbu.vy1@gmail.com>
2023-02-27 14:58:35 +0530

    Scripts

--- scripts/Deployment.ts
+++ scripts/Deployment.ts
@@ -3,46 +3,44 @@ import * as dotenv from 'dotenv';
 import { Ballot__factory } from "../typechain-types";
 dotenv.config();
 
-const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
-
-const convertStringArrayToBytes32Array = (stringArray: string[]) => {
-    return stringArray.map(prop => ethers.utils.formatBytes32String(prop));
-}
 
 async function main() {
-    const provider = new ethers.providers.AlchemyProvider('goerli', process.env.ALCHEMY_API_KEY);
-    const privateKey = process.env.PRIVATE_WALLET_KEY;
-    if (!privateKey || privateKey.length <= 0) {
-        throw new Error("No private key provided");
-    }
 
-    const wallet = new ethers.Wallet(privateKey); // By passing the key we are using our actual metamask wallet
+    const provider = new ethers.providers.InfuraProvider(
+        "goerli",
+        process.env.INFURA_API_KEY);
+    const privateKey = process.env.PRIVATE_KEY;
+
+    if(!privateKey || privateKey.length<=0)
+      throw new Error("Private Key Not Found");
+    
+    const wallet = new ethers.Wallet(privateKey);
     const signer = wallet.connect(provider);
-    console.log("🚀 ~ file: Deployment.ts:14 ~ main ~ privateKey:", privateKey)
-    console.log( {provider} );
-
-    const lastBlock = await provider.getBlock('latest');
-    console.log({lastBlock});
-
 
     const balance = await signer.getBalance();
-    console.log('The balance of the signer is: ', balance.toString());
- 
-    const proposals = PROPOSALS;
-    if (proposals.length <= 0) {
-        throw new Error("No proposals provided");
-    }
+    console.log(`The balance of address ${signer.address}: ${balance}`);
+
+    const args  = process.argv;
+    const proposals = args.slice(2);
+    if(proposals.length<=0) 
+      throw new Error("Missing args");
+
+    console.log("Proposals: ");
+    proposals.forEach((element, index) => {
+    console.log(`Proposal N. ${index + 1}: ${element}`);
+    });
 
     const ballotContractFactory = new Ballot__factory(signer);
-    const ballotContract = await ballotContractFactory.deploy(
-        convertStringArrayToBytes32Array(proposals)
-    );
-    const txReceipt = await ballotContract.deployTransaction.wait(); // similar to ballotContract.deployed() in truffle
 
-    console.log('The ballot contract was deployed at address: ', ballotContract.address);
-    console.log('The transaction hash is: ', txReceipt.transactionHash);
-  // TODO
-}
+    console.log("Deploying ballot contract");
+    const ballotContract = await ballotContractFactory.deploy(
+        proposals.map((proposal)=>ethers.utils.formatBytes32String(proposal))
+      );
+    console.log("Awaiting Deploying ballot contract");
+    const tx = await ballotContract.deployTransaction.wait();
+    console.log(`Deploying ballot contract at address ${ballotContract.address} in the block number ${tx.blockNumber}`);
+
+    }
 
 main().catch((error) => {
   console.error(error);
---
+++ scripts/QueryResults.ts
@@ -0,0 +1,31 @@
+import { ethers } from "ethers";
+import * as dotenv from 'dotenv';
+import { Ballot__factory } from "../typechain-types";
+dotenv.config();
+
+
+async function main(){
+    const provider = new ethers.providers.InfuraProvider(
+        "goerli",
+        process.env.INFURA_API_KEY);
+    const privateKey = process.env.PRIVATE_KEY;
+
+    if(!privateKey || privateKey.length<=0)
+            throw new Error("Private Key Not Found");
+
+    const wallet = new ethers.Wallet(privateKey);
+    const signer = wallet.connect(provider);
+
+    const contract = "0x76C3eE533bA35b40FB12Fa8ed1ba70Fa665Af7BF";
+    const ballotContractFactory = new Ballot__factory(signer);
+    const ballotContract = await ballotContractFactory.attach(contract);
+    
+    const winner = await ballotContract.winnerName();
+    const winningProposal = await ballotContract.winningProposal();
+    console.log(`The winner of the voting is Proposal ${winningProposal.toNumber()+1}: ${ethers.utils.parseBytes32String(winner)}`);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
